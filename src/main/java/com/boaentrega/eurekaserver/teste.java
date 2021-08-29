9190b74e1846418d3ccd5bf2dca7504b923fa686
brunoav91@gmail.com <brunoav91@gmail.com>
2021-08-29 15:00:03 -0300

    add java 11 deploy

---
+++ src/main/java/com/boaentrega/eurekaserver/teste.java
@@ -0,0 +1,17 @@
+package com.boaentrega.eurekaserver;
+
+public class teste {
+
+    public static void main(String[] args) {
+
+        for(int i=0; i <= 1000; i++){
+        //    System.out.println( "Numero" + i);
+
+            if(i % 2 == 0){
+                System.out.println( "PAR" + i);
+            }else{
+                System.out.println("IMPAR" + i);
+            }
+        }
+    }
+}
